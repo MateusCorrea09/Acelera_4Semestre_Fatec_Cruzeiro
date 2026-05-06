3e11fd08d5ea4c31c75b4a03480576a4c1daaf13
MateusCorrea03 <mateus.contato09@outlook.com>
2026-05-05 21:41:29 -0300

    ajustes para o linux

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import HomeProfessor from './container/HomeProfessor'
 import RegistroUsuario from './container/RegistroUsuario'
 import Login from './container/Login'
 import Homealuno from './container/Homealuno'
+import ManagerTurma from './container/ManagerTurma'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MyGlobalStyles />
-    <Login/>
+    <ManagerTurma/>
 
   </React.StrictMode>,
 )
